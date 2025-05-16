7d62af5e3ffdeba601c905a576f9e2b712460e0a
pranavvengatesh <pranavvengatesh191103@gmail.com>
2025-05-16 22:26:12 +0530

    Fix: Add fontawesome dependency

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/BLOG/', 
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        external: ['@fortawesome/fontawesome-free']
+      },
+    },
+  },
 })
