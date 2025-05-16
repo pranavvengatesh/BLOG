e9fcefd663c0b9606491657bcca8f126db88c95e
pranavvengatesh <pranavvengatesh191103@gmail.com>
2025-05-16 22:38:53 +0530

    Fix: Add fontawesome dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar"
 import NewsArea from "./components/NewsArea"
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
   const [category, setCategory] = useState("general");
